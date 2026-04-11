e849b9ce99b64dd613ddc26e8637869758ae2d72
thanathip srisawatpattana <thanathip.sr@irecruit.co.th>
2026-04-12 00:57:38 +0700

    fix: benchmark:node

--- src/benchmark/apps/http.ts
+++ src/benchmark/apps/http.ts
@@ -7,8 +7,14 @@ export const AppHttp = ({ name , port , message }: {
 }) => {
    
     const server = http.createServer((req, res) => {
-        res.end(message);
-        return;
+        if (req.url === "/" && req.method === "GET") {
+            return res.end(message);
+        } 
+
+        res.statusCode = 404;
+
+        return res.end("Not Found");
+        
     });
 
     server.listen(port , () => console.log(`Server '${name}' running at : http://localhost:${port}`))
