4c6ab534729a27cd2323d5d6ce8c3208c0338120
thanathip srisawatpattana <thanathip.sr@irecruit.co.th>
2026-05-11 10:52:09 +0700

    add: pre routes

---
+++ src/lib/core/compiler/pre-routes.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+// AUTO GENERATED FILE
+// DO NOT EDIT
+
+export interface AppRoutes {}
+
+export type AppRoute = keyof AppRoutesE:\npm\tspace-spear\src\lib\core\compiler\pre-routes.ts
