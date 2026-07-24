315bb7a808714fd3b5db8ab73567f126a45568e7
thanathip srisawatpattana <thanathip.sr@irecruit.co.th>
2026-07-24 17:17:19 +0700

    feat: adapter for test cases

---
+++ __tests__/app/adapter.ts
@@ -0,0 +1,20 @@
+import uWS from "uWebSockets.js";
+import net from "net";
+import http from "http";
+
+export type AdapterType = 'http' | 'uws' | 'net';
+
+export function getAdapter() {
+  const adapterEnv = process.env.TEST_ADAPTER || 'http';
+  
+  switch (adapterEnv.toLowerCase()) {
+    case 'uws':
+      return { adapter: uWS, portOffset: 100, type: 'uws' as AdapterType };
+    case 'net':
+      return { adapter: net, portOffset: 200, type: 'net' as AdapterType };
+    case 'http':
+    default:
+      return { adapter: http, portOffset: 0, type: 'http' as AdapterType };
+  }
+}
+
